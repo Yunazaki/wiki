7e5f5d97188c7c8a3e640e8ac0ab76837cb2882f
Euna <66821940+Yunazaki@users.noreply.github.com>
2023-12-30 07:32:49 -0700

    add page.html
    Added page.html and implemented functionality

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,17 @@
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
 from . import util
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def page(request, title):
+    if not util.get_entry(title):
+        return HttpResponseNotFound()
+    
+    return render(request, "encyclopedia/page.html", {
+        "entry": util.get_entry(title)
+    })
